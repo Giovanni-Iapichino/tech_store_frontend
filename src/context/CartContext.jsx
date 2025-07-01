94981b895553296240f45f7fea3a2e4da38d5887
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-07-01 15:33:27 +0200

    add context cart

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,57 @@
+import { Children, createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+export const useCart = () => useContext(cartContext);
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prev) => {
+      const existing = prev.find((item) => item.id === product.id);
+
+      if (existing) {
+        return prev.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        return [...prev, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const updateQuantity = (id, quantity) => {
+    setCart((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, quantity: Math.max(1, quantity) } : item
+      )
+    );
+  };
+
+  const clearCart = () => {
+    setCart, [];
+  };
+
+  const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        updateQuantity,
+        clearCart,
+        total,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
