dc93f1ef61cb29986c79c77882b5c85d1da12435
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-07-01 12:24:13 +0200

    add smartphone card

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,9 +1,33 @@
+import { useProducts } from "../context/GetProductsContext";
+
 export default function ShopPage() {
+  const { products, loading, error } = useProducts();
+
+  if (loading) return <p>Caricamento in corso...</p>;
+  if (error) return <p>{error}</p>;
   return (
     <>
-      <main>
-        <h1 className="my-5">Shop</h1>
-      </main>
+      <div className="container">
+        <h2 className="mb-4">
+          <strong>Smartphone disponibili</strong>
+        </h2>
+        <div className="row g-3">
+          {products.map((product) => (
+            <div key={product.id} className="col-4">
+              <div className="card h-100 shadow p-3">
+                <div className="card-body d-flex flex-column"></div>
+                <h4 className="card-title">
+                  {`${product.brand} ${product.title} ${product.model}`}
+                </h4>
+                <p className="mt-auto card-text">
+                  <strong>€{product.price}</strong>
+                </p>
+                <p className="mt-auto card-text">{product.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
