0d879df17e510acdb532f880fa492b81f9a0b2ba
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-07-02 11:02:13 +0200

    add cart local storage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,10 +1,18 @@
-import { createContext, useContext, useState } from "react";
+import { parse } from "@fortawesome/fontawesome-svg-core";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 export const useCart = () => useContext(CartContext);
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    const storedCart = localStorage.getItem("cart");
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   const addToCart = (product) => {
     setCart((prev) => {
