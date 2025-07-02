9ecb6289dc9775f316d164b173ee69ed8f1af4ce
thomasraimondi <thomas.raimondi14@gmail.com>
2025-07-02 17:44:05 +0200

    edit menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ export default function Navbar() {
               </li>
               <li className="nav-item">
                 <NavLink className="nav-link" to="/shop">
-                  Promozioni
+                  Offerte
                 </NavLink>
               </li>
               <li className="nav-item">
