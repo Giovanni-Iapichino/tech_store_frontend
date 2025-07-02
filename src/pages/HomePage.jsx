d8f53a58695d5f6ee7bd038d6598d4a1b16001fd
thomasraimondi <thomas.raimondi14@gmail.com>
2025-07-02 15:09:24 +0200

    move carusel out of container

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,8 +10,8 @@ export default function HomePage() {
   const { bestSeller, promotions } = useProducts();
 
   return (
-    <main className="container">
-      <div id="carouselExampleIndicators" className="carousel slide my-5 h-50" data-bs-ride="carousel">
+    <>
+      <div id="carouselExampleIndicators" className="carousel slide h-30" data-bs-ride="carousel">
         {/* Immagini */}
         <div className="carousel-inner">
           <div className="carousel-item active">
@@ -42,96 +42,98 @@ export default function HomePage() {
           <span className="visually-hidden">Successiva</span>
         </button>
       </div>
-      <div className="services my-5">
-        <div className="row">
-          <div className="col-md-3">
-            <div className="service d-flex gap-3">
-              <div className="service-icon">
-                <FontAwesomeIcon className="fs-3" style={{ color: "#ff6543" }} icon={faCartShopping} />
+      <main className="container">
+        <div className="services my-5">
+          <div className="row">
+            <div className="col-md-3">
+              <div className="service d-flex gap-3">
+                <div className="service-icon">
+                  <FontAwesomeIcon className="fs-3" style={{ color: "#ff6543" }} icon={faCartShopping} />
+                </div>
+                <div className="service-content">
+                  <h5>Spedizione Gratuita</h5>
+                  <p>Spedizione gratuita per tutti gli ordini superiori a 100€</p>
+                </div>
               </div>
-              <div className="service-content">
-                <h5>Spedizione Gratuita</h5>
-                <p>Spedizione gratuita per tutti gli ordini superiori a 100€</p>
+            </div>
+            <div className="col-md-3">
+              <div className="service d-flex gap-3">
+                <div className="service-icon">
+                  <FontAwesomeIcon className="fs-3" style={{ color: "#ff6543" }} icon={faTrophy} />
+                </div>
+                <div className="service-content">
+                  <h5>Qualità</h5>
+                  <p>Prodotti originali e di alta qualità</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="service d-flex gap-3">
+                <div className="service-icon">
+                  <FontAwesomeIcon className="fs-3" style={{ color: "#ff6543" }} icon={faClipboardList} />
+                </div>
+                <div className="service-content">
+                  <h5>Offerte</h5>
+                  <p>Offerte e sconti sui nostri prodotti</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="service d-flex gap-3">
+                <div className="service-icon">
+                  <FontAwesomeIcon className="fs-3" style={{ color: "#ff6543" }} icon={faShieldHalved} />
+                </div>
+                <div className="service-content">
+                  <h5>Sicurezza</h5>
+                  <p>Pagamento sicuro con PayPal, Carta di Credito, Bonifico Bancario</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-md-3">
-            <div className="service d-flex gap-3">
-              <div className="service-icon">
-                <FontAwesomeIcon className="fs-3" style={{ color: "#ff6543" }} icon={faTrophy} />
-              </div>
-              <div className="service-content">
-                <h5>Qualità</h5>
-                <p>Prodotti originali e di alta qualità</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="service d-flex gap-3">
-              <div className="service-icon">
-                <FontAwesomeIcon className="fs-3" style={{ color: "#ff6543" }} icon={faClipboardList} />
-              </div>
-              <div className="service-content">
-                <h5>Offerte</h5>
-                <p>Offerte e sconti sui nostri prodotti</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="service d-flex gap-3">
-              <div className="service-icon">
-                <FontAwesomeIcon className="fs-3" style={{ color: "#ff6543" }} icon={faShieldHalved} />
-              </div>
-              <div className="service-content">
-                <h5>Sicurezza</h5>
-                <p>Pagamento sicuro con PayPal, Carta di Credito, Bonifico Bancario</p>
-              </div>
-            </div>
+        </div>
+        <div className="brands my-5">
+          <div className="row align-items-center gap-3 justify-content-center">
+            <Link to="/shop?brand=apple" className="card-brand border">
+              <img className="w-100" src="/apple.jpg" alt="Brand 1" />
+            </Link>
+            <Link to="/shop?brand=samsung" className="card-brand border">
+              <img className="w-100" src="/samsung.jpg" alt="Brand 1" />
+            </Link>
+            <Link to="/shop?brand=lg" className="card-brand border">
+              <img className="w-100" src="/lg.png" alt="Brand 1" />
+            </Link>
+            <Link to="/shop?brand=huawei" className="card-brand border">
+              <img className="w-100" src="/huawei.png" alt="Brand 1" />
+            </Link>
+            <Link to="/shop?brand=xiaomi" className="card-brand border">
+              <img className="w-100" src="/xiaomi.png" alt="Brand 1" />
+            </Link>
+            <Link to="/shop?brand=motorola" className="card-brand border">
+              <img className="w-100" src="/motorola.svg" alt="Brand 1" />
+            </Link>
           </div>
         </div>
-      </div>
-      <div className="brands my-5">
-        <div className="row align-items-center gap-3 justify-content-center">
-          <Link to="/shop?brand=apple" className="card-brand border">
-            <img className="w-100" src="/apple.jpg" alt="Brand 1" />
-          </Link>
-          <Link to="/shop?brand=samsung" className="card-brand border">
-            <img className="w-100" src="/samsung.jpg" alt="Brand 1" />
-          </Link>
-          <Link to="/shop?brand=lg" className="card-brand border">
-            <img className="w-100" src="/lg.png" alt="Brand 1" />
-          </Link>
-          <Link to="/shop?brand=huawei" className="card-brand border">
-            <img className="w-100" src="/huawei.png" alt="Brand 1" />
-          </Link>
-          <Link to="/shop?brand=xiaomi" className="card-brand border">
-            <img className="w-100" src="/xiaomi.png" alt="Brand 1" />
-          </Link>
-          <Link to="/shop?brand=motorola" className="card-brand border">
-            <img className="w-100" src="/motorola.svg" alt="Brand 1" />
-          </Link>
+        <h5 className="my-5">Promotions</h5>
+        <div className="promotions my-5 d-flex gap-4 justify-content-between align-items-center">
+          <div className="banner">
+            <FontAwesomeIcon className="fs-3 cursor-pointer" icon={faChevronLeft} />
+          </div>
+          <div className="row gap-3">{promotions && promotions.map((product) => <ProductCardLigth key={product.id} product={product} />)}</div>
+          <div className="banner-arrow">
+            <FontAwesomeIcon className="fs-3" icon={faChevronRight} />
+          </div>
         </div>
-      </div>
-      <h5 className="my-5">Promotions</h5>
-      <div className="promotions my-5 d-flex gap-4 justify-content-between align-items-center">
-        <div className="banner">
-          <FontAwesomeIcon className="fs-3 cursor-pointer" icon={faChevronLeft} />
+        <h5 className="my-5">Best Selling Items</h5>
+        <div className="promotions my-5 d-flex gap-3 justify-content-between align-items-center">
+          <div className="banner">
+            <FontAwesomeIcon className="fs-3 cursor-pointer" icon={faChevronLeft} />
+          </div>
+          <div className="row gap-4">{bestSeller && bestSeller.map((product) => <ProductCardLigth key={product.id} product={product} />)}</div>
+          <div className="banner-arrow">
+            <FontAwesomeIcon className="fs-3" icon={faChevronRight} />
+          </div>
         </div>
-        <div className="row gap-3">{promotions && promotions.map((product) => <ProductCardLigth key={product.id} product={product} />)}</div>
-        <div className="banner-arrow">
-          <FontAwesomeIcon className="fs-3" icon={faChevronRight} />
-        </div>
-      </div>
-      <h5 className="my-5">Best Selling Items</h5>
-      <div className="promotions my-5 d-flex gap-3 justify-content-between align-items-center">
-        <div className="banner">
-          <FontAwesomeIcon className="fs-3 cursor-pointer" icon={faChevronLeft} />
-        </div>
-        <div className="row gap-4">{bestSeller && bestSeller.map((product) => <ProductCardLigth key={product.id} product={product} />)}</div>
-        <div className="banner-arrow">
-          <FontAwesomeIcon className="fs-3" icon={faChevronRight} />
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
