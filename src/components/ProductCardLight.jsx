59518781f0dbae088a14de9384a67bbbec9b3a2c
thomasraimondi <thomas.raimondi14@gmail.com>
2025-07-02 11:52:32 +0200

    edit filed in card

--- src/components/ProductCardLight.jsx
+++ src/components/ProductCardLight.jsx
@@ -3,40 +3,54 @@ import { faCartShopping, faHeart } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 export default function ProductCardLigth({ product }) {
+  if (product.promotions) {
+    const promotion_in_corso = product.promotions.filter((promotion) => promotion.promo_state === "in corso");
+    if (promotion_in_corso.length > 0) {
+      product.promotions = promotion_in_corso[0];
+    } else {
+      product.promotions = product.promotions[0];
+    }
+  }
+
+  console.log(product.promotions);
+
   return (
-    <Link className="col-2 border rounded-3 overflow-hidden d-flex flex-column justify-content-center align-items-center gap-3 promotion-item position-relative" to={`/detailsproduct/${product.id}`}>
-      <div className="position-absolute top-0 end-0">
-        {product.discount && (
-          <div className="promotion-item-content d-flex flex-column gap-2">
-            {" "}
-            <span>{product.discount}%</span>
+    <Link
+      className="text-dark col-2 border rounded-3 overflow-hidden d-flex flex-column justify-content-around align-items-center gap-3 promotion-item position-relative text-decoration-none"
+      to={`/detailsproduct/${product.id}`}
+    >
+      <div className="position-absolute top-0 end-0 p-1">
+        {product.promotions && (
+          <div className="fs-6 bg-danger text-white rounded-3 p-1">
+            <span>{product.promotions.discount}%</span>
           </div>
         )}
       </div>
-      {product.discount && (
-        <div className="position-absolute top-0 start-0">
-          {product.promo_state === "in corso" ? (
+      {product.promotions && (
+        <div className="promo_state position-absolute top-0 start-0 p-1">
+          {product.promotions.promo_state === "in corso" ? (
             <div className="promotion-item-content d-flex flex-column gap-2">
               <span>in corso</span>
             </div>
           ) : (
             <div className="promotion-item-content d-flex flex-column gap-2">
-              <span>dal {product.start_date}</span>
+              <span>dal {new Date(product.promotions.start_date).toLocaleDateString()}</span>
             </div>
           )}
         </div>
       )}
-      <div className="">
-        <img className="w-100 h-100" src="/smartphone_placeholder.jpeg" alt={product.title} />
+      <div className="mt-3">
+        <img className="w-100 h-100" src="/smartphone_placeholder.jpeg" alt={product.title[0].toUpperCase() + product.title.slice(1)} />
       </div>
       <div className="promotion-item-content d-flex flex-column gap-2">
-        <span>{product.title}</span>
-        {product.discount_price ? (
-          <span>
-            <span className="text-decoration-line-through">{product.price}€</span> <span>{product.discount_price}€</span>
+        <span className="text-center">{product.title[0].toUpperCase() + product.title.slice(1)}</span>
+        {product.promotions ? (
+          <span className="d-flex flex-row align-items-center justify-content-center gap-2 w-100">
+            <span className="original-price text-decoration-line-through">{product.price}€</span>
+            <span className="discounted-price text-danger fw-bold">{product.promotions.discount_price}€</span>
           </span>
         ) : (
-          <span>{product.price}€</span>
+          <></>
         )}
       </div>
       <div className="action-buttons gap-2 position-absolute bottom-50% end-50% p-3">
