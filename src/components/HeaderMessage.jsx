bd08198a947c883eaed7546bfd445794999c6274
Nicolapsq <nicolapasqua84@gmail.com>
2025-07-03 15:52:52 +0200

    filtro in navbar

--- src/components/HeaderMessage.jsx
+++ src/components/HeaderMessage.jsx
@@ -1,7 +1,7 @@
 export default function HeaderMessage({text}){
     return (
         <div className="text-center mb-4">
-            <h1 className="text-danger display-4">{text}</h1>
+            <h1 className="display-4" style={{ color: "#ff6543" }}>{text}</h1>
         </div>
     )
 }
