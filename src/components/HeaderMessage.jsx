bebc538eb05eb538799d2eceb92624dd868ee49c
Nicolapsq <nicolapasqua84@gmail.com>
2025-07-03 13:35:59 +0200

    add compare and dump component

---
+++ src/components/HeaderMessage.jsx
@@ -0,0 +1,7 @@
+export default function HeaderMessage({text}){
+    return (
+        <div className="text-center mb-4">
+            <h1 className="text-danger display-4">{text}</h1>
+        </div>
+    )
+}
--- src/pages/ComparisonPage.jsx
+++ src/pages/ComparisonPage.jsx
@@ -1,9 +1,46 @@
+import { useCompare } from "../context/CompareContext";
+import { Link } from "react-router-dom";
+
 export default function ComparisonPage() {
+  const { compareList, removeFromCompare, clearCompare } = useCompare();
+
+  if (compareList.length === 0)
+    return (
+      <div className="container">
+        <p>Nessun prodotto selezionato per il confronto.</p>
+        <Link className="btn btn-warning mt-4" to="/shop">
+          Torna allo Shop
+        </Link>
+      </div>
+    );
+
   return (
-    <>
-      <main>
-        <h1 className="my-5">ComparisonPage</h1>
-      </main>
-    </>
+    <div className="container">
+      <h1 className="my-4">Confronto Prodotti</h1>
+      <div className="d-flex gap-3 overflow-auto">
+        {compareList.map((product) => (
+          <div
+            className="card p-3 shadow"
+            key={product.id}
+            style={{ minWidth: "300px" }}
+          >
+            <h5>{product.title}</h5>
+            <p>Modello: {product.model}</p>
+            <p>Prezzo: €{product.price}</p>
+            <p>Brand: {product.brand}</p>
+            <p>OS: {product.operating_system}</p>
+            <button
+              className="btn btn-sm btn-danger"
+              onClick={() => removeFromCompare(product.id)}
+            >
+              Rimuovi
+            </button>
+          </div>
+        ))}
+      </div>
+      <button className="btn btn-warning mt-4" onClick={clearCompare}>
+        Svuota confronto
+      </button>
+    </div>
   );
 }
