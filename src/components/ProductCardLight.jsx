81c00ee6099f70740e9e9fd50e23adcb6ab1de79
thomasraimondi <thomas.raimondi14@gmail.com>
2025-07-04 08:30:08 +0200

    edit product card add quantity and compare

--- src/components/ProductCardLight.jsx
+++ src/components/ProductCardLight.jsx
@@ -1,9 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCartShopping,
-  faHeart,
-  faTrashCan,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faHeart, faTrashCan, faMinus, faPlus, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import { faCircleCheck as faCircleCheckRegular } from "@fortawesome/free-regular-svg-icons";
 import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext";
 import { useWishlist } from "../context/WishlistContext";
@@ -11,11 +8,12 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useCompare } from "../context/CompareContext"; // Importo il contesto per la comparazione dei prodotti
 
-export default function ProductCardLigth({ product }) {
-  const { addToCart, removeFromCart, cart } = useCart();
+export default function ProductCardLigth({ product, isInCompare, addToCompare, removeFromCompare }) {
+  const { addToCart, removeFromCart, cart, updateQuantity } = useCart();
   const [isInCart, setIsInCart] = useState(false);
   const { addToWishlist, removeFromWishlist, wishlist } = useWishlist();
   const [isInWishlist, setIsInWishlist] = useState(false);
+  const [quantity, setQuantity] = useState(cart.find((item) => item.id === product.id)?.quantity || 1);
 
   const location = useLocation();
   const isShopPage = location.pathname === "/shop";
@@ -35,9 +33,10 @@ export default function ProductCardLigth({ product }) {
   return (
     <>
       <Link
-        className="text-dark col-2 border rounded-3 overflow-hidden d-flex flex-column justify-content-around align-items-center gap-3 promotion-item position-relative text-decoration-none mx-auto"
+        className="text-dark col-2 border rounded-3 d-flex flex-column justify-content-around align-items-center gap-3 promotion-item position-relative text-decoration-none mx-auto"
         to={`/shop/${product.id}`}
       >
+        {/* Promotion */}
         <div className="position-absolute top-0 end-0 p-1">
           {product.promotion && (
             <div className="fs-6 bg-danger text-white rounded-3 p-1">
@@ -45,7 +44,7 @@ export default function ProductCardLigth({ product }) {
             </div>
           )}
         </div>
-        {product.promotion && (
+        {/* {product.promotion && (
           <div className="promo_state position-absolute top-0 start-0 p-1">
             {product.promotion.promo_state === "in corso" ? (
               <div className="promotion-item-content d-flex flex-column gap-2">
@@ -53,33 +52,24 @@ export default function ProductCardLigth({ product }) {
               </div>
             ) : (
               <div className="promotion-item-content d-flex flex-column gap-2">
-                <span>
-                  dal{" "}
-                  {new Date(product.promotion.start_date).toLocaleDateString()}
-                </span>
+                <span>dal {new Date(product.promotion.start_date).toLocaleDateString()}</span>
               </div>
             )}
           </div>
-        )}
+        )} */}
+
+        {/* Image */}
         <div className="mt-3">
-          <img
-            className="w-100 h-100"
-            src="/smartphone_placeholder.jpeg"
-            alt={product.title[0].toUpperCase() + product.title.slice(1)}
-          />
+          <img className="w-100 h-100" src="/smartphone_placeholder.jpeg" alt={product.title[0].toUpperCase() + product.title.slice(1)} />
         </div>
+
+        {/* Title and price */}
         <div className="promotion-item-content d-flex flex-column gap-2">
-          <span className="text-center">
-            {product.title[0].toUpperCase() + product.title.slice(1)}
-          </span>
+          <span className="text-center">{product.title[0].toUpperCase() + product.title.slice(1)}</span>
           {product.promotion && (
             <span className="d-flex flex-row align-items-center justify-content-center gap-2 w-100">
-              <span className="original-price text-decoration-line-through">
-                {product.price}€
-              </span>
-              <span className="discounted-price text-danger fw-bold">
-                {product.promotion.discount_price}€
-              </span>
+              <span className="original-price text-decoration-line-through">{product.price}€</span>
+              <span className="discounted-price text-danger fw-bold">{product.promotion.discount_price}€</span>
             </span>
           )}
           {isShopPage && (
@@ -88,92 +78,166 @@ export default function ProductCardLigth({ product }) {
             </span>
           )}
         </div>
-        <div className="action-buttons gap-2 position-absolute bottom-50% end-50% p-3">
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              if (isInCart) {
-                removeFromCart(product.id);
-                setIsInCart(false);
-              } else {
+
+        {/* Action buttons */}
+        <div className="action-buttons gap-2 position-absolute bottom-50% end-50% p-3 d-flex align-items-center justify-content-center">
+          {isInCart ? (
+            <div className="gap-2 w-100 d-flex flex-column align-items-center justify-content-center">
+              <button
+                className="btn rounded-circle p-2 d-flex align-items-center justify-content-center position-absolute"
+                style={{ backgroundColor: "#ff6543", height: "30px", width: "30px", top: "-20px", left: "22px" }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  if (quantity === 1) {
+                    removeFromCart(product.id);
+                    setIsInCart(false);
+                  } else {
+                    setQuantity(quantity - 1);
+                    updateQuantity(product.id, parseInt(quantity) - 1);
+                  }
+                }}
+              >
+                {quantity > 1 ? (
+                  <FontAwesomeIcon
+                    style={{
+                      height: "15px",
+                      width: "15px",
+                      padding: "10px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                    icon={faMinus}
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    style={{
+                      height: "15px",
+                      width: "15px",
+                      padding: "10px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                    icon={faTrashCan}
+                  />
+                )}
+              </button>
+              <div
+                className="btn rounded-circle p-2 d-flex align-items-center justify-content-center "
+                style={{
+                  height: "40px",
+                  width: "40px",
+                  backgroundColor: "#ff6543",
+                }}
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                {quantity}
+              </div>
+              <button
+                className="btn rounded-circle p-2 d-flex align-items-center justify-content-center position-absolute"
+                style={{ backgroundColor: "#ff6543", height: "30px", width: "30px", top: "62px", left: "22px" }}
+              >
+                <FontAwesomeIcon
+                  icon={faPlus}
+                  style={{
+                    height: "15px",
+                    width: "15px",
+                    padding: "10px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setQuantity(quantity + 1);
+                    updateQuantity(product.id, parseInt(quantity) + 1);
+                  }}
+                />
+              </button>
+            </div>
+          ) : (
+            <FontAwesomeIcon
+              className="btn rounded-circle p-2 d-flex align-items-center justify-content-center"
+              style={{
+                height: "20px",
+                width: "20px",
+                backgroundColor: "#ff6543",
+              }}
+              icon={faCartShopping}
+              onClick={(e) => {
+                e.preventDefault();
                 addToCart(product);
                 setIsInCart(true);
-              }
-            }}
-            className="btn rounded-circle p-2"
-            style={{ backgroundColor: "#ff6543" }}
-          >
-            {isInCart ? (
-              <FontAwesomeIcon
-                className="fs-3 cursor-pointer "
-                style={{
-                  height: "20px",
-                  width: "20px",
-                  padding: "10px",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-                icon={faTrashCan}
-              />
-            ) : (
-              <FontAwesomeIcon
-                className="fs-3 cursor-pointer"
-                style={{
-                  height: "20px",
-                  width: "20px",
-                  padding: "10px",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-                icon={faCartShopping}
-              />
-            )}
-          </button>
-          <button
-            className="btn rounded-circle p-2"
-            style={{ backgroundColor: "#ff6543" }}
-            onClick={(e) => {
-              e.preventDefault();
-              if (isInWishlist) {
+              }}
+            />
+          )}
+          {isInWishlist ? (
+            <FontAwesomeIcon
+              className="btn rounded-circle p-2 d-flex align-items-center justify-content-center"
+              style={{
+                height: "20px",
+                width: "20px",
+                backgroundColor: "#ff6543",
+              }}
+              icon={faTrashCan}
+              onClick={(e) => {
+                e.preventDefault();
                 removeFromWishlist(product.id);
                 setIsInWishlist(false);
-              } else {
+              }}
+            />
+          ) : (
+            <FontAwesomeIcon
+              className="btn rounded-circle p-2 d-flex align-items-center justify-content-center"
+              style={{
+                height: "20px",
+                width: "20px",
+                backgroundColor: "#ff6543",
+              }}
+              icon={faHeart}
+              onClick={(e) => {
+                e.preventDefault();
                 addToWishlist(product);
                 setIsInWishlist(true);
-              }
-            }}
-          >
-            {isInWishlist ? (
+              }}
+            />
+          )}
+        </div>
+
+        {/* Compare button */}
+        {isShopPage && (
+          <div className="compare-button position-absolute d-flex align-items-center justify-content-center gap-2" style={{ top: "10px", left: "10px" }}>
+            {isInCompare ? (
               <FontAwesomeIcon
-                className="fs-3 cursor-pointer"
-                style={{
-                  height: "20px",
-                  width: "20px",
-                  padding: "10px",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
+                style={{ height: "20px", width: "20px" }}
+                className="text-success"
+                icon={faCircleCheck}
+                onClick={(e) => {
+                  e.preventDefault();
+                  removeFromCompare(product.id);
                 }}
-                icon={faTrashCan}
               />
             ) : (
               <FontAwesomeIcon
-                className="fs-3 cursor-pointer"
-                style={{
-                  height: "20px",
-                  width: "20px",
-                  padding: "10px",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
+                style={{ height: "20px", width: "20px" }}
+                icon={faCircleCheckRegular}
+                onClick={(e) => {
+                  e.preventDefault();
+                  addToCompare(product);
                 }}
-                icon={faHeart}
               />
             )}
-          </button>
-        </div>
+            <span style={{ fontSize: "10px" }}>Compare</span>
+          </div>
+        )}
+
+        {/* Scadenza */}
+        {/* <div className="position-absolute gap-2" style={{ top: "-50px", left: "10px" }}>
+          <span style={{ fontSize: "10px" }}>Scade il 31/12/2025</span>
+        </div> */}
       </Link>
     </>
   );
