ce5085ebd04539108f672f20947e67a64d6e9383
thomasraimondi <thomas.raimondi14@gmail.com>
2025-07-05 15:18:17 +0200

    create component link

---
+++ src/components/MenuLink.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+
+export default function MenuLink() {
+  return (
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <NavLink className="nav-link" aria-current="page" to="/">
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/shop">
+          Offerte
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/shop">
+          Shop
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
