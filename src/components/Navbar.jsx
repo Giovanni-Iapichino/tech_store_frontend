60a287d88fc633d2ff7e1d63328c64c0358c1a31
thomasraimondi <thomas.raimondi14@gmail.com>
2025-07-05 17:20:04 +0200

    change position search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,6 +64,29 @@ export default function Navbar() {
           <div className="justify-content-center align-items-center d-none d-sm-flex col-6 col-md-4">
             <MenuLink />
           </div>
+          {/* Menu centrato */}
+          <div className="collapse justify-content-center d-sm-none" id="navbarNav">
+            <MenuLink />
+          </div>
+          <div className="d-flex flex-column flex-md-row col-12 col-md-3 justify-content-center align-items-center">
+            {/* FORM di ricerca dentro la navbar */}
+            <form onSubmit={handleSubmit} className="d-flex justify-content-center align-items-center np-form w-100 mt-2 mt-md-0">
+              <input type="text" className="form-control border-0 shadow-none np-input" placeholder="Cerca..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+              <button
+                className="btn"
+                type="submit"
+                style={{
+                  background: "#ff6543",
+                  color: "#fff",
+                  borderRadius: "0 25px 25px 0",
+                  border: "none",
+                  padding: "8px 14px",
+                }}
+              >
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </button>
+            </form>
+          </div>
           <div className="d-flex flex-md-row-reverse flex-grow-1 gap-3 col-12 col-sm-2 justify-content-center align-items-center">
             {/* Bottoni */}
             <div className="buttons d-flex gap-3">
@@ -75,21 +98,21 @@ export default function Navbar() {
                 )}
 
                 {/* {wishlistItems > 0 && (
-                <span
-                  className="badge bg-dark position-absolute rounded-circle"
-                  style={{
-                    top: "-10px",
-                    right: "-10px",
-                    width: "20px",
-                    height: "20px",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  {wishlistItems}
-                </span>
-              )} */}
+                      <span
+                        className="badge bg-dark position-absolute rounded-circle"
+                        style={{
+                          top: "-10px",
+                          right: "-10px",
+                          width: "20px",
+                          height: "20px",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                        }}
+                      >
+                        {wishlistItems}
+                      </span>
+                    )} */}
               </Link>
               <div className="position-relative mt-2">
                 <span onClick={() => setShowCart(!showCart)} style={{ cursor: "pointer" }}>
@@ -117,29 +140,6 @@ export default function Navbar() {
               </div>
               <FontAwesomeIcon style={{ color: "#ff6543" }} className="fs-4 mt-2" icon={faUser} />
             </div>
-          </div>
-          {/* Menu centrato */}
-          <div className="collapse justify-content-center d-sm-none" id="navbarNav">
-            <MenuLink />
-          </div>
-          <div className="d-flex flex-column flex-md-row col-12 col-md-3 justify-content-center align-items-center">
-            {/* FORM di ricerca dentro la navbar */}
-            <form onSubmit={handleSubmit} className="d-flex justify-content-center align-items-center np-form w-100 mt-2 mt-md-0">
-              <input type="text" className="form-control border-0 shadow-none np-input" placeholder="Cerca..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
-              <button
-                className="btn"
-                type="submit"
-                style={{
-                  background: "#ff6543",
-                  color: "#fff",
-                  borderRadius: "0 25px 25px 0",
-                  border: "none",
-                  padding: "8px 14px",
-                }}
-              >
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-              </button>
-            </form>
           </div>
         </div>
       </nav>
