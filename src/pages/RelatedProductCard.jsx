6e8a23cbaf5f31e479fe9a2b0e4f7baade7dcf8b
thomasraimondi <thomas.raimondi14@gmail.com>
2025-07-06 07:52:08 +0200

    add tolowercase to selected products brand

--- src/pages/RelatedProductCard.jsx
+++ src/pages/RelatedProductCard.jsx
@@ -9,62 +9,82 @@ import { useParams } from "react-router-dom";
 import { useCart } from "../context/CartContext";
 
 const RelatedProducts = () => {
-    const { addToWishlist } = useWishlist();  
-    const { addToCart } = useCart(); 
-    const [related, setRelated] = useState([]);
-    const { slug } = useParams();                                                                         // ottieni lo slug del prodotto dalla URL
+  const { addToWishlist } = useWishlist();
+  const { addToCart } = useCart();
+  const [related, setRelated] = useState([]);
+  const { slug } = useParams(); // ottieni lo slug del prodotto dalla URL
 
-    useEffect(() => {                                                                                     // effettua la chiamata API per ottenere i prodotti correlati
-        const fetchRelatedProducts = async () => {                                                           //utilizza async/await per gestire le chiamate API
-            try {
-                const response = await axios.get(`http://localhost:3000/api/v1/products`);                      // ottieni tutti i prodotti
-                setRelated(response.data.products); 
-               
-                const resProduct = await axios.get(`http://localhost:3000/api/v1/products/${slug}`);             // ottieni il prodotto selezionato
-                const selectedProduct = resProduct.data.product;                                               // destruttura la risposta per ottenere il prodotto selezionatos
-                const relatedProducts = response.data.products.filter(product =>                             // filtra i prodotti correlati in base alla marca del prodotto selezionato
-                    product.brand === selectedProduct.brand && product.id !== selectedProduct.id
-                );
-                setRelated(relatedProducts.slice(0, 4));                                                       //limitare a 4 prodotti correlati
-            }   catch (error) {
-                console.error("Error fetching related products:", error);
-            }
-        };
-        fetchRelatedProducts();
-    }, [setRelated]);
-    return (
-        <div className="related-products">
-            <div className="row">
-                {related.map(product => (
-                    <div key={product.id} className="col-md-2 mb-2">
-                        <div className="card">
-                            <img src="../smartphone_placeholder.jpeg" alt="smartphone" className="card-img-top" />
-                            <div className="card-body">
-                                <h6 className="card-title">{product.brand} {product.title} {product.model}</h6>
-                                <p className="card-text" style={{
-                                    color: " #089408",
-                                    fontWeight: "bold",
-                                }}><FontAwesomeIcon icon={faEuroSign} />{product.price}</p>
-                                <button className="btn px-0"onClick={()=> addToCart(product)} >
-                                    <FontAwesomeIcon icon={faCartShopping} className="text-black fs-5" style={{
-                                        background: "#ff6543",
-                                        padding: "8px",
-                                        borderRadius: "50%",
-                                    }} />
-                                </button>
-                                <button className="btn px-1"onClick={()=> addToWishlist(product)} >
-                                    <FontAwesomeIcon icon={faHeart} className="text-black fs-5" style={{
-                                        background: "#ff6543",
-                                        padding: "8px",
-                                        borderRadius: "50%",
-                                    }} />
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+  useEffect(() => {
+    // effettua la chiamata API per ottenere i prodotti correlati
+    const fetchRelatedProducts = async () => {
+      //utilizza async/await per gestire le chiamate API
+      try {
+        const response = await axios.get(`http://localhost:3000/api/v1/products`); // ottieni tutti i prodotti
+        setRelated(response.data.products);
+
+        const resProduct = await axios.get(`http://localhost:3000/api/v1/products/${slug}`); // ottieni il prodotto selezionato
+        const selectedProduct = resProduct.data.product; // destruttura la risposta per ottenere il prodotto selezionatos
+        const relatedProducts = response.data.products.filter(
+          (
+            product // filtra i prodotti correlati in base alla marca del prodotto selezionato
+          ) => product.brand === selectedProduct.brand.toLowerCase() && product.id !== selectedProduct.id
+        );
+        setRelated(relatedProducts.slice(0, 4)); //limitare a 4 prodotti correlati
+      } catch (error) {
+        console.error("Error fetching related products:", error);
+      }
+    };
+    fetchRelatedProducts();
+  }, [setRelated]);
+  return (
+    <div className="related-products">
+      <div className="row gap-3">
+        {related.map((product) => (
+          <div key={product.id} className="col-md-2 mb-2">
+            <div className="card">
+              <img src="../smartphone_placeholder.jpeg" alt="smartphone" className="card-img-top" />
+              <div className="card-body">
+                <h6 className="card-title">
+                  {product.brand} {product.title} {product.model}
+                </h6>
+                <p
+                  className="card-text"
+                  style={{
+                    color: " #089408",
+                    fontWeight: "bold",
+                  }}
+                >
+                  <FontAwesomeIcon icon={faEuroSign} />
+                  {product.price}
+                </p>
+                <button className="btn px-0" onClick={() => addToCart(product)}>
+                  <FontAwesomeIcon
+                    icon={faCartShopping}
+                    className="text-black fs-5"
+                    style={{
+                      background: "#ff6543",
+                      padding: "8px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                </button>
+                <button className="btn px-1" onClick={() => addToWishlist(product)}>
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    className="text-black fs-5"
+                    style={{
+                      background: "#ff6543",
+                      padding: "8px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                </button>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default RelatedProducts;
