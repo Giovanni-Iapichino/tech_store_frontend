c7ed4a5a6e6d6d377647c3665b41d9ddccc20791
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-07-09 12:23:21 +0200

    fix quantity

--- src/pages/DetailsProductPage.jsx
+++ src/pages/DetailsProductPage.jsx
@@ -2,7 +2,13 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useCart } from "../context/CartContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEuroSign, faArrowLeft, faHeart, faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEuroSign,
+  faArrowLeft,
+  faHeart,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 import { useWishlist } from "../context/WishlistContext";
 import RelatedProducts from "./RElatedProductCard";
 import axios from "axios";
@@ -10,24 +16,27 @@ import { useNewsletter } from "../context/newsletterContext";
 import PopUpNewsletter from "../components/PopUpNewsletter";
 import { useToast } from "../context/ToastContext";
 
-
 export default function DetailsProductPage() {
-  const { addToCart, cart } = useCart();                              //aggiungi al carrello
-  const [product, setProduct] = useState();                             //prodotto da visualizzare
-  const [quantity, setQuantity] = useState(1);                          // quantità del prodotto da aggiungere al carrello
-  const [ isInCart, setIsInCart] = useState(cart.some((item) => item.id === product?.id) || false);
-  const [isBuy, setIsBuy] = useState(cart.some((item) => item.id === product?.id) || false);
+  const { addToCart, cart } = useCart(); //aggiungi al carrello
+  const [product, setProduct] = useState(); //prodotto da visualizzare
+  const [quantity, setQuantity] = useState(1); // quantità del prodotto da aggiungere al carrello
+  const [isInCart, setIsInCart] = useState(
+    cart.some((item) => item.id === product?.id) || false
+  );
+  const [isBuy, setIsBuy] = useState(
+    cart.some((item) => item.id === product?.id) || false
+  );
   const [isAddCart, setIsAddCart] = useState(false);
-
 
   const { addToWishlist } = useWishlist(); //aggiungi alla wishlist
   const { slug } = useParams();
-  const { randomClick, updateRandomClick, open, setOpen, newsletter } = useNewsletter();
+  const { randomClick, updateRandomClick, open, setOpen, newsletter } =
+    useNewsletter();
   const navigate = useNavigate(); //per navigare tra le pagine
   const { showToast } = useToast();
-  const{ updateQuantity }= useCart();
+  const { updateQuantity } = useCart();
 
-  const productApiUrl = `http://localhost:3000/api/v1` + "/products/" + slug;          // URL dell'API per ottenere il prodotto specifico
+  const productApiUrl = `http://localhost:3000/api/v1` + "/products/" + slug; // URL dell'API per ottenere il prodotto specifico
 
   useEffect(() => {
     if (newsletter === "false") {
@@ -36,22 +45,19 @@ export default function DetailsProductPage() {
     }
   }, []);
 
-  
-   useEffect(() =>{
-    if(!cart.some((item) => item.id === product?.id)) {
-       setIsAddCart(false);
-       setIsBuy(false);
-       setIsInCart(false);
+  useEffect(() => {
+    if (!cart.some((item) => item.id === product?.id)) {
+      setIsAddCart(false);
+      setIsBuy(false);
+      setIsInCart(false);
     }
   }, [cart, product]);
-
 
   useEffect(() => {
     if (randomClick === 0) {
       setOpen(true); // apre il pop-up della newsletter se il numero di click casuali
     }
   }, [randomClick, setOpen]);
-
 
   const fetchProduct = () => {
     axios.get(productApiUrl).then((res) => {
@@ -62,15 +68,12 @@ export default function DetailsProductPage() {
   };
   useEffect(fetchProduct, []); // chiama la funzione fetchProduct al caricamento del componente
 
-
   useEffect(() => {
     setIsInCart(cart.some((item) => item.id === product?.id) || false);
-    if(cart.some((item) => item.id === product?.id)){
+    if (cart.some((item) => item.id === product?.id)) {
       setIsBuy(false);
-    };
-  },[product])
-  
-
+    }
+  }, [product]);
 
   return (
     <>
@@ -117,14 +120,19 @@ export default function DetailsProductPage() {
                         <span className="discount">
                           <span className="text-muted fs-6">
                             <del>
-                              <FontAwesomeIcon icon={faEuroSign} /> {product.price}
+                              <FontAwesomeIcon icon={faEuroSign} />{" "}
+                              {product.price}
                             </del>{" "}
                             -{product.discount}%
                           </span>
                         </span>
                         <span>
                           <p className="price" style={{ color: "#ff0000" }}>
-                            <FontAwesomeIcon icon={faEuroSign} /> {(product.price - (product.price * product.discount) / 100).toFixed(2)}
+                            <FontAwesomeIcon icon={faEuroSign} />{" "}
+                            {(
+                              product.price -
+                              (product.price * product.discount) / 100
+                            ).toFixed(2)}
                           </p>
                         </span>
                       </>
@@ -142,20 +150,36 @@ export default function DetailsProductPage() {
                   <div className="button">
                     {isInCart && (
                       <div className="d-flex align-items-center mb-2">
-                        <button className="btn btn-outline-secondary"
+                        <button
+                          className="btn btn-outline-secondary"
                           onClick={() => {
-                            updateQuantity(product.id, cart.find((item) => item.id === product.id)?.quantity - 1);
+                            updateQuantity(
+                              product.id,
+                              cart.find((item) => item.id === product.id)
+                                ?.quantity - 1
+                            );
                           }}
                           style={{ minWidth: "36px" }}
                         >
                           <FontAwesomeIcon icon={faMinus} />
                         </button>
-                        <span className="mx-2" style={{ minWidth: "32px", textAlign: "center" }}>
-                          {cart.find((item) => item.id === product.id)?.quantity}
+                        <span
+                          className="mx-2"
+                          style={{ minWidth: "32px", textAlign: "center" }}
+                        >
+                          {
+                            cart.find((item) => item.id === product.id)
+                              ?.quantity
+                          }
                         </span>
-                        <button className="btn btn-outline-secondary"
+                        <button
+                          className="btn btn-outline-secondary"
                           onClick={() => {
-                          updateQuantity(product.id, cart.find((item) => item.id === product.id)?.quantity + 1);
+                            updateQuantity(
+                              product.id,
+                              cart.find((item) => item.id === product.id)
+                                ?.quantity + 1
+                            );
                           }}
                           style={{ minWidth: "36px" }}
                         >
@@ -166,17 +190,19 @@ export default function DetailsProductPage() {
                     <button
                       className="btn btn-success m-1 p-2"
                       onClick={() => {
-                        if(isBuy){
+                        if (isBuy || isInCart) {
                           navigate("/checkout");
                         } else {
-                          addToCart(product);
+                          if (!isInCart) {
+                            addToCart(product);
+                          }
                           setIsInCart(true);
                           setIsBuy(true);
                         }
                       }}
                       disabled={isAddCart}
                     >
-                      {isBuy?"conferma": "acquista"}
+                      {isBuy ? "conferma" : "acquista"}
                     </button>
                     <button
                       className="btn m-1 p-2"
@@ -188,7 +214,9 @@ export default function DetailsProductPage() {
                         addToCart(product);
                         showToast("Prodotto aggiunto al carrello");
                         setIsInCart(true);
-                        setQuantity(cart.find((item) =>(item.id === product.id ))?.quantity);
+                        setQuantity(
+                          cart.find((item) => item.id === product.id)?.quantity
+                        );
                         setIsAddCart(true);
                       }}
                       disabled={isBuy}
@@ -224,22 +252,53 @@ export default function DetailsProductPage() {
             <div className="container-form">
               <nav>
                 <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                  <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button " role="tab" aria-controls="nav-home" aria-selected="true">
+                  <button
+                    className="nav-link active"
+                    id="nav-home-tab"
+                    data-bs-toggle="tab"
+                    data-bs-target="#nav-home"
+                    type="button "
+                    role="tab"
+                    aria-controls="nav-home"
+                    aria-selected="true"
+                  >
                     Descrizione
                   </button>
-                  <button className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">
+                  <button
+                    className="nav-link"
+                    id="nav-profile-tab"
+                    data-bs-toggle="tab"
+                    data-bs-target="#nav-profile"
+                    type="button"
+                    role="tab"
+                    aria-controls="nav-profile"
+                    aria-selected="false"
+                  >
                     Scheda tecnica
                   </button>
                 </div>
               </nav>
               <div className="tab-content" id="nav-tabContent">
-                <div className="tab-pane fade show active p-3" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabIndex={0}>
+                <div
+                  className="tab-pane fade show active p-3"
+                  id="nav-home"
+                  role="tabpanel"
+                  aria-labelledby="nav-home-tab"
+                  tabIndex={0}
+                >
                   {product.description}
                 </div>
-                <div className="tab-pane fade p-3" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabIndex={0}>
+                <div
+                  className="tab-pane fade p-3"
+                  id="nav-profile"
+                  role="tabpanel"
+                  aria-labelledby="nav-profile-tab"
+                  tabIndex={0}
+                >
                   <ul>
                     <li>
-                      <strong>Sistema operativo:</strong> {product.operating_system}
+                      <strong>Sistema operativo:</strong>{" "}
+                      {product.operating_system}
                     </li>
                     <li>
                       <strong>Ram:</strong> {product.ram}
